cc4393bf71f2cddf928c2d0c6f35792d453124f5
Debarati Patwari <debarati.official20@gmail.com>
2022-11-10 11:32:27 +0530

    Create details.js
    This is the dashboard page.

---
+++ pages/components/details.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+const Details = () => {
+  const router = useRouter();
+
+  const [logindata, setLogindata] = useState([]);
+  console.log("from state", logindata);
+  const Matchdata = () => {
+    const getuser = localStorage.getItem("User_all_data");
+
+    if (getuser && getuser.length) {
+      const user = JSON.parse(getuser);
+
+      console.log(user);
+      setLogindata(user);
+    }
+  };
+
+  const userlogout = () => {
+    localStorage.removeItem("User_all_data");
+    router.push("/components/loginformik");
+  };
+
+  useEffect(() => {
+    Matchdata();
+  }, []);
+
+  return (
+    <>
+      {logindata.length === 0 ? (
+        "errror"
+      ) : (
+        <>
+          <h1>Welcome {logindata.name}</h1>
+          <button onClick={userlogout}>Logout</button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Details;
