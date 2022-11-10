093d744aa5287565b8323201b62e3f5770bd45b1
Debarati Patwari <debarati.official20@gmail.com>
2022-11-10 11:34:07 +0530

    Update index.js
    index.js js page is upadted .

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Loginformik from './components/loginformik'
+
 
 export default function Home() {
   return (
-    <>
-        <h2>Home page</h2>
-    </> 
+   <>
+      
+    
+      <Loginformik/> 
+
+    </>
   )
 }
-
