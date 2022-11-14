9fd5fe4443fa1e93883930bb56b5b35f8c711ee3
srinu14121999 <srinath.laha@brainiuminfotech.com>
2022-11-14 12:37:06 +0530

    Update Read.js
    .

--- pages/components/Read.js
+++ pages/components/Read.js
@@ -1,79 +1,73 @@
-import axios from "axios";
-import Link from "next/link";
+import axios from 'axios'
+import Link from 'next/link'
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
 const Read = () => {
-  const [data, setData] = useState([]); //To store the data which we get from api as a array
 
-  function getData() {
-    axios
-      .get("https://636deabdb567eed48accca4e.mockapi.io/EmployeePortal")
-      .then((res) => {
-        // console.log(res.data)
-        setData(res.data);
-      });
-  }
+      const [data, setStore] = useState([]) //To store the data which we get from api as a array 
 
-  function handelDelete(id) {
-      axios
-        .delete(`https://636deabdb567eed48accca4e.mockapi.io/EmployeePortal/${id}`)
-        .then(() => {
-          getData();
-        });
-    }
+      function getData() {
+            axios.get('https://636deabdb567eed48accca4e.mockapi.io/EmployeePortal')
+                  .then((res) => {
+                        // console.log(res.data)
+                        setStore(res.data)
+                  }
 
-  useEffect(() => {
-    getData();
-  }, []); //whenever data is changed useeffect works
+                  )
+      }
+      useEffect(() => {
 
-  return (
-    <div>
-      <h1>Employee Details</h1>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Aplication For</th>
-            <th scope="col">Current Date</th>
-            <th scope="col">Reason</th>
-            <th scope="col">Apply Date</th>
-            <th scope="col">Reporting Head</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        {data.map((accessData) => {
-          return (
-            <>
-              <tbody>
-                <tr>
-                  <th scope="row">{accessData.id}</th>
-                  <td>{accessData.date}</td>
-                  <td>{accessData.aplication}</td>
-                  <td>{accessData.reporting_head}</td>
-                  <td>{accessData.date}</td>
-                  <td>{accessData.reason}</td>
-                  <td>
-                    <button className="btn btn-success">Edit</button>
-                  </td>
-                  <td scope="col">
-                    <button
-                      type="button"
-                      class="btn btn-danger"
-                      onClick={() => handelDelete(accessData.id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            </>
-          );
-        })}
-      </table>
-      <Link href={"/components/Create"}>GO To Employee Form Page</Link>
-    </div>
-  );
-};
+            getData()
+      }, [])  //whenever data is changed useeffect works 
 
-export default Read;
+
+
+
+      return (
+            <div>
+                  <h1>Employee Details</h1>
+                  <table class="table">
+                        <thead>
+                              <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Aplication For</th>
+                                    <th scope="col">Current Date</th>
+                                    <th scope="col">Reason</th>
+                                    <th scope="col">Apply Date</th>
+                                    <th scope="col">Reporting Head</th>
+                                    <th scope="col">Action</th>
+                              </tr>
+                        </thead>
+                        {data.map((accessData) => {
+                              return (
+                                    <>
+                                          <tbody>
+                                                <tr>
+                                                      <th scope="row">{accessData.id}</th>
+                                                      <td>{accessData.aplication}</td>
+                                                      <td>{accessData.date}</td>
+                                                      <td>{accessData.reason}</td>
+                                                      <td>{accessData.date}</td>
+                                                      <td>{accessData.reporting_head}</td>
+                                                      <td ><button className='btn btn-success'>Edit</button></td>
+                                                      <td><button className='btn btn-danger '>Delete</button></td>
+                                                </tr>
+
+                                          </tbody>
+
+                                    </>
+                              )
+                        }
+
+
+                        )
+
+                        }
+                  </table>
+                  <Link href={'/components/Create'}>GO To Employee Form Page</Link>
+            </div>
+      )
+}
+
+export default Read
